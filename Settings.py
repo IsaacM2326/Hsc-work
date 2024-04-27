34f39db4b69ca19c2e04c3fc53ac6842f6781687
Isaac Muthalakuzhiyil <muthalakuzhiyil.isaac@gmail.com>
2024-04-27 11:53:56 +1000

    changing themes complete

--- Settings.py
+++ Settings.py
@@ -1,17 +1,28 @@
 import customtkinter as ctk
-root 
-ctk.set_appearance_mode('dark','light') 
-ctk.set_default_color_theme('blue')
- 
-def set_appearance_mode('dark','light')
-#window 
+
 root = ctk.CTk()
-root.title ('customtkinter app')
+
+themes = [
+    {'bg': 'black', 'fg': 'white', 'text': 'Light Theme'},
+    {'bg': 'white', 'fg': 'black', 'text': 'Dark Theme'},
+    {'bg': '#333', 'fg': 'white', 'text': 'white dove'},
+    {'bg': 'black', 'fg': 'light green', 'text': 'Custom Theme 2'},
+    {'bg': '#grey', 'fg': 'grey', 'text': 'east eyes  3'}
+]
+
+current_theme_index = 0
+
+def switch_theme():
+    global current_theme_index
+    current_theme_index = (current_theme_index + 1) % len(themes)
+    theme = themes[current_theme_index]
+    root.configure(bg=theme['bg'])
+    root.configure(fg_color=theme['fg'], text=theme['text'])
+
+root.title('customtkinter app')
 root.geometry('600x400')
 
-button = ctk.CTkButton(root, text = 'change theme  ',
-                       fg_color= '#FF0',
-                       text_color= '#000',
-                       command = lambda: ctk.set_appearance_mode('light','dark'))
-button.pack(pady=80)
+button1 = ctk.CTkButton(root, text='change theme', command=switch_theme)
+button1.pack(pady=80)
+
 root.mainloop()
