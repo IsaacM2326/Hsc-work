544000292b08e6568b07f3d502456f1c8f564375
Isaac Muthalakuzhiyil <muthalakuzhiyil.isaac@gmail.com>
2024-04-28 11:05:08 +1000

    trying to undersand switching pages and finsih settings

--- Settings.py
+++ Settings.py
@@ -1,6 +1,7 @@
 import customtkinter as ctk
-root = ctk.CTk()
 
+root = ctk.CTk()
+root.theme = {'fg_color': '#FF0', 'text_color': '#000'}
 
 themes = [
     {'bg': 'black', 'fg': 'white', 'text': 'Light Theme'},
@@ -10,27 +11,19 @@ themes = [
     {'bg': '#4287f5', 'fg': 'white', 'text': 'Custom Theme 3'}
 ]
 
-current_theme_index=0
+current_theme_index = 0
+
 def switch_theme():
     global current_theme_index
-    if current_theme_index >= len(themes):
-        current_theme_index = 0
+    current_theme_index = (current_theme_index + 1) % len(themes)
     theme = themes[current_theme_index]
-    root.configure(bg=theme['bg'], fg=theme['fg'])
-    button1.config(text=theme['text'])
+    root.configure(bg=theme['bg'])
+    root.configure(fg_color=theme['fg'], text=theme['text'])
 
-
- 
-
-
-#root 
-root = ctk.CTk()
-root.title ('customtkinter app')
+root.title('customtkinter app')
 root.geometry('600x400')
 
-button1 = ctk.CTkButton(root, text = 'change theme  ',
-                       fg_color= '#FF0',
-                       text_color= '#000',
-                       command = lambda: switch_theme()) 
+button1 = ctk.CTkButton(root, text='change theme', fg_color=root.theme['fg_color'], text_color=root.theme['text_color'], command=switch_theme)
 button1.pack(pady=80)
+
 root.mainloop()
