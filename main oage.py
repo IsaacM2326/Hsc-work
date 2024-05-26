f4014a87aba32a8688bc8d6fde6ad275b6c7c294
IsaacM2326 <138081024+IsaacM2326@users.noreply.github.com>
2024-05-26 18:50:12 +1000

    2.3.8
    Got rid of unnecessary lines

--- main oage.py
+++ main oage.py
@@ -29,7 +29,7 @@ def resize_and_open_page():
     for p in pages:
         p.pack_forget()
 
-    page_4.pack(pady=100)
+    resize_page.pack(pady=100)
 
    
     w = width_entry.get()
@@ -64,7 +64,7 @@ physics_questions = [
     ("What is the push or pull on an object that can cause it to accelerate called?", "Force",'F--ce'),
     ("What is the unit of measure for force?", "Newton",'starts with N'),
     ("What is the sum of all forces acting on an object called?", "Net force", 'N--, F----'),
-    ("What is the unit of measure for force?", "Newton",'starts with N'),
+    ("Is light faster than sound  ?", "Yes ",'yes or no '),
     ("Newton's First Law of Motion is also called:", "Law of Inertia",'Law of I--rt--'),
     ("Newton's first Law of Motion states that if there is no net force acting on an object it will:", " remain at rest",'re---- a- re--'),
     (" In the equation F = ma, what does m represent?", "Mass", 'M---'),
@@ -73,14 +73,9 @@ physics_questions = [
     ("Are mass and weight the same?", "No", 'Yes/No')
 ]
 
-physics_hints = [('less than 5 ', 'less than 9 ','Fo---', 'Ne----', 'N--, F----', '--N', 're---- a- re--','6---','3-m/s2 ', 'Yes/No' )
 
-
-
-]
-count2 = len(physics_questions)
-count2_hint = len(physics_hints)
-current_question_index = 1
+physics_count = len(physics_questions)
+current_question_index = 0
 
 # Widgets for page 2
 physics_entry = ctk.CTkEntry(physics_page, width=300)
@@ -89,8 +84,8 @@ physics_entry.place (relx=.45,rely=.3)
 answer_label = ctk.CTkLabel(physics_page, text='', font=('Helvetica', 12))
 answer_label.place (relx=.5,rely=.1)
 
-my_label1 = ctk.CTkLabel(physics_page, text='Question', font=('Helvetica', 24))
-my_label1.place (relx=.2,rely=.2)
+physics_label = ctk.CTkLabel(physics_page, text='Question', font=('Helvetica', 24))
+physics_label.place (relx=.2,rely=.2)
 
 hint_label1 = ctk.CTkLabel(physics_page, text='', font=('Helvetica', 12))
 hint_label1.place (relx=.5,rely=.25)
@@ -99,7 +94,7 @@ hint_label1.place (relx=.5,rely=.25)
 
 
 def submit():
-    if 0 <= current_question_index < count2:
+    if 0 <= current_question_index < physics_count:
         answer = physics_entry.get().strip().capitalize()  # Get and clean user input
         correct_answer = physics_questions[current_question_index][1].capitalize()
 
@@ -114,19 +109,19 @@ def submit():
 
 
 def hint1():
-    if 0 <= current_question_index < count2:
+    if 0 <= current_question_index < physics_count:
         hint_label1.configure(text=f'Hint: {physics_questions[current_question_index][2]}')
     else:
         hint_label1.configure(text='No question to get a hint')
     
 
 
-
+# increments the question index and reset the labels 
 def next_question_physics():
    
     global current_question_index
-    current_question_index = randint(0, count2 - 1)
-    my_label1.configure(text=physics_questions[current_question_index][0])
+    current_question_index = randint(0, physics_count - 1)
+    physics_label.configure(text=physics_questions[current_question_index][0])
     answer_label.configure(text='')
     hint_label1.configure(text='')
     quiz_label1.configure(text='')
@@ -171,12 +166,13 @@ biology_questions = [
     ("What is the process of cell division called?", "Mitosis",'Mi-----'),
     ("What are the main components of the central nervous system?", "Brain and Spinal Cord",'Br--- and S----- C---'),
 ]
-count1 = len(biology_questions)
-current_question_index = 1
+biology_count = len(biology_questions)
+current_question_index = 0
 
 #======================defining quiz commands =============================================================================
-def submit2():
-    if 0 <= current_question_index < count1:
+#compares the input fro entry to the array 
+def biology_submit():
+    if 0 <= current_question_index < biology_count:
         answer = biology_entry.get().strip().capitalize()  # Get and clean user input
         correct_answer = biology_questions[current_question_index][1].capitalize()
 
@@ -188,8 +184,8 @@ def submit2():
     else:
         quiz_label2.configure(text='No question to answer!')
 
-def hint2():
-    if 0 <= current_question_index < count1:
+def biology_hintss():
+    if 0 <= current_question_index < biology_count:
         hint_label.configure(text=f'Hint: {biology_questions[current_question_index][2]}')
     else:
         hint_label.configure(text='No question to get a hint')
@@ -198,8 +194,8 @@ def next_question_biology():
     biology_entry.delete(0, ctk.END)  # Clear the entry widget's content
     global current_question_index
     global current_question_index
-    current_question_index = randint(0, count1 - 1)
-    my_label.configure(text=biology_questions[current_question_index][0])
+    current_question_index = randint(0, biology_count - 1)
+    biology_label.configure(text=biology_questions[current_question_index][0])
     answer_label.configure(text='')
     hint_label.configure(text='')
     quiz_label2.configure(text='')
@@ -209,16 +205,16 @@ def next_question_biology():
 quiz_label2 = ctk.CTkLabel(biology_page, text='Biology quiz ')
 quiz_label2.place (relx=.5,rely=.1)
 
-my_label = ctk.CTkLabel(biology_page, text='', font=('Helvetica',24))
-my_label.place(relx=.35,rely=.2)
+biology_label = ctk.CTkLabel(biology_page, text='', font=('Helvetica',24))
+biology_label.place(relx=.35,rely=.2)
 
 
-enter_button = ctk.CTkButton(biology_page, text='Enter', command=submit2)
+enter_button = ctk.CTkButton(biology_page, text='Enter', command=biology_submit)
 enter_button.place (relx=.5,rely=.35)
 
 
 
-hint_button = ctk.CTkButton(biology_page, text='Hint', command=hint2)
+hint_button = ctk.CTkButton(biology_page, text='Hint', command=biology_hintss)
 hint_button.place (relx=.5,rely=.4)
 
 next_button = ctk.CTkButton(biology_page, text='Next Question', command=next_question_biology)
@@ -234,14 +230,14 @@ quiz_buttons_2 = [next_button ,  enter_button, hint_button]
 
 #=====================================PAGE  3  END ========================================================================================================================
 #==========================PAGE $ START==================================================================================================================================================== 
-page_4= ctk.CTkFrame(main_frame ,width =1500, height =800)
+resize_page= ctk.CTkFrame(main_frame ,width =1500, height =800)
 
 def resize_and_open_page():# gets inputs and adjusts page 
     
     for p in pages:
         p.pack_forget()
 
-    page_4.pack(pady=100)
+    resize_page.pack(pady=100)
 
    
     w = width_entry.get()
@@ -249,30 +245,30 @@ def resize_and_open_page():# gets inputs and adjusts page
     root.geometry(f'{w}x{h}')
 
 
-my_button = ctk.CTkButton(page_4, text='Resize', command=resize_and_open_page)
+my_button = ctk.CTkButton(resize_page, text='Resize', command=resize_and_open_page)
 
-width_label = ctk.CTkLabel(page_4,text='Width')
+width_label = ctk.CTkLabel(resize_page,text='Width')
 width_label.place(relx=.5,rely=.1)
-width_entry=ctk. CTkEntry(page_4)
+width_entry=ctk. CTkEntry(resize_page)
 width_entry.place(relx=.5,rely=.3)
 
-height_label = ctk.CTkLabel(page_4,text='Height')
+height_label = ctk.CTkLabel(resize_page,text='Height')
 height_label.place(relx=.5,rely=.4)
 
-height_entry=ctk.CTkEntry(page_4)
+height_entry=ctk.CTkEntry(resize_page)
 height_entry.place(relx=.5,rely=.45)
 
-warning_label= ctk.CTkLabel(page_4,text='Minimum w= 800, h=800' )
+warning_label= ctk.CTkLabel(resize_page,text='Minimum w= 800, h=800' )
 warning_label.place(relx=.5, rely=.5)
 
 
 
-page_4.place(relx=.5,rely=.5)
+resize_page.place(relx=.5,rely=.5)
 
 #=========================================PAGE 4 END ======================================================================================
 #=========================================SWITCH PAGES ===================================
 
-pages=[start_page, physics_page, biology_page, page_4]
+pages=[start_page, physics_page, biology_page, resize_page]
 count=0
 
 def move_next_page():
@@ -312,7 +308,7 @@ bottom_frame.place(rely=.95, relx=0.45)
 Settings_frame= ctk.CTkFrame(root)
 
 
-root.theme = {'fg_color': '#FF0', 'text_color': '#000'}
+
 
 #=============================================================Defining inclusive features ===============================================
 
@@ -341,7 +337,7 @@ def switch_theme():
     start_page.configure(bg_color=theme['bg'], fg_color=theme['fg'], )
     physics_page.configure(bg_color=theme['bg'], fg_color=theme['fg'], )
     biology_page.configure(bg_color=theme['bg'], fg_color=theme['fg'], )
-    page_4.configure(bg_color=theme['bg'], fg_color=theme['fg'], )
+    resize_page.configure(bg_color=theme['bg'], fg_color=theme['fg'], )
     main_frame.configure(bg_color=theme['bg'], fg_color=theme['fg'], )
     bottom_frame.configure(bg_color=theme['bg'], fg_color=theme['fg'], )
     root.configure(bg_color=theme['bg'], fg_color=theme['fg'], )
@@ -364,9 +360,9 @@ def increase_font_size():
 
 def update_fonts():
     # Update the fonts in all labels
-    my_label1.configure(font=(selected_font, selected_font_size))
+    physics_label.configure(font=(selected_font, selected_font_size))
     hint_label1.configure(font=(selected_font, selected_font_size))
-    my_label.configure(font=(selected_font, selected_font_size))
+    biology_label.configure(font=(selected_font, selected_font_size))
     hint_label.configure(font=(selected_font, selected_font_size))
     height_label.configure(font=(selected_font, selected_font_size))
     width_label.configure(font=(selected_font, selected_font_size))
@@ -382,6 +378,7 @@ def update_fonts():
         button.configure (font=(selected_font, selected_font_size))
     for button in setting_buttons:
         button.configure(font=(selected_font, selected_font_size))
+        
         
 
 
@@ -400,8 +397,8 @@ def create_widgets():
     global setting_buttons
     change_button = ctk.CTkButton(Settings_frame, text="Change Style", command=change_font_style)
     change_button.grid(row=2, column=2)
-    button2 = ctk.CTkButton(Settings_frame,  text='Change theme', fg_color=root.theme['fg_color'], text_color=root.theme['text_color'], command=switch_theme)
-    button2.grid(row=2, column=1)
+    change_theme_button = ctk.CTkButton(Settings_frame,  text='Change theme',  command=switch_theme)
+    change_theme_button.grid(row=2, column=1)
     
     font_button = ctk.CTkButton( Settings_frame, text="Increase Font Size", command=increase_font_size)
     font_button.grid(row=2, column=0)
@@ -409,7 +406,7 @@ def create_widgets():
     font_button = ctk.CTkButton( Settings_frame, text="Resize ", command=resize_and_open_page)
     font_button.grid(row=2, column=3)
 
-    setting_buttons = [change_button, button2, font_button]
+    setting_buttons = [change_button, change_theme_button, font_button]
 
    
 
